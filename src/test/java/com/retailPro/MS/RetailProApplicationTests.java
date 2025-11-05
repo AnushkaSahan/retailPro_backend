8aad58692f02b52f48f7f4b745d62f74a24b0080
Anushka Sahan <anushkasahan200439@gmail.com>
2025-11-05 10:48:19 +0530

    Initial commit

---
+++ src/test/java/com/retailPro/MS/RetailProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retailPro.MS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
