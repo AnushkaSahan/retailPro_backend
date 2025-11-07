b54a2a808c50c273b64b335c293e95cb7b7365dc
Anushka Sahan <anushkasahan200439@gmail.com>
2025-11-07 20:22:23 +0530

    feact: add new 3 repos for profile, settings and notification

---
+++ src/main/java/com/retailpro/repository/UserProfileRepository.java
@@ -0,0 +1,11 @@
+package com.retailpro.repository;
+
+import com.retailpro.model.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+    Optional<UserProfile> findByUserId(Long userId);
+}
